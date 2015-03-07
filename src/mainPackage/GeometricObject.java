a3b0bbe8d139b310b4c219fecae20cd3c445222b
Tommy Kananen <tkananen@udel.edu>
2015-03-07 18:34:21 -0500

    drtyu

---
+++ src/mainPackage/GeometricObject.java
@@ -0,0 +1,51 @@
+package mainPackage;
+
+public class GeometricObject {
+	double side1 = 1.0;
+	double side2 = 1.0;
+	double side3 = 1.0;
+	
+	
+	
+	public GeometricObject(){
+	TriangleClass triangle = new TriangleClass();
+		
+		
+	}
+	
+	public GeometricObject(double side1, double side2, double side3) {
+		super();
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+		TriangleClass triangle = new TriangleClass(side1, side2, side3);
+	}
+
+	public double getSide1() {
+		return side1;
+	}
+
+	public void setSide1(double side1) {
+		this.side1 = side1;
+	}
+
+	public double getSide2() {
+		return side2;
+	}
+
+	public void setSide2(double side2) {
+		this.side2 = side2;
+	}
+
+	public double getSide3() {
+		return side3;
+	}
+
+	public void setSide3(double side3) {
+		this.side3 = side3;
+	}
+	public static double getArea(double side1, double side2, soublde
+	
+	
+
+}
---
+++ src/mainPackage/TriangleClass.java
@@ -0,0 +1,6 @@
+package mainPackage;
+
+public class TriangleClass extends GeometricObject {
+	
+
+}
