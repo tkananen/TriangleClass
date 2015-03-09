4303a6944fa65856f3c2c60a2ac1388fc215af76
tkananen <tkananen@hotmail.com>
2015-03-09 16:20:00 -0400

    finalized....?

--- src/mainPackage/Triangle.java
+++ src/mainPackage/Triangle.java
@@ -1,15 +1,15 @@
 package mainPackage;
 
-public abstract class TriangleClass extends  GeometricObject {
+public class Triangle extends GeometricObject{
 	double side1 = 1.0;
 	double side2 = 1.0;
 	double side3 = 1.0;
 	double peri = getPerimeter();
+
+	public Triangle(){
 	
-	public TriangleClass(){
-		
 	}
-	public TriangleClass(double side1, double side2, double side3){
+	public Triangle(double side1, double side2, double side3){
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
@@ -23,28 +23,23 @@ public abstract class TriangleClass extends  GeometricObject {
 	public double getSide3() {
 		return side3;
 	}
-	public void setSide1(double side1) {
-		this.side1 = side1;
-	}
-	public void setSide2(double side2) {
-		this.side2 = side2;
-	}
-	public void setSide3(double side3) {
-		this.side3 = side3;
-	}
+
+
 	public double getArea(){
-				
+			
 		double area = Math.pow((peri * ((peri-side1)*(peri-side2)*(peri-side3))), (1/2));
 		return area;
-			
-		
+	
 	}
 	public double getPerimeter(){
 		return ((side1 + side2 + side3)/2);
 	}
-	
+
+	public String toString() {
+		return "TriangleClass [side1= " + side1 + ", side2= " + side2
+			+ ", side3= " + side3 + ", peri= " + peri + "]";
 }
 
-	
+}
 
 
